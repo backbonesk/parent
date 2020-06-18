8262251fffac05077e00675564b1dc32aba355d1
Nono3551 <Nono3551@gmail.com>
2020-06-18 16:28:40 +0200

    changed base repository

--- app/src/main/java/sk/backbone/android/shared/repositories/server/BaseServerRepository.kt
+++ app/src/main/java/sk/backbone/android/shared/repositories/server/BaseServerRepository.kt
@@ -1,8 +1,19 @@
 package sk.backbone.android.shared.repositories.server
 
 import android.content.Context
+import sk.backbone.android.shared.repositories.server.client.BaseHttpRequest
+import sk.backbone.android.shared.repositories.server.client.HttpClient
 import sk.backbone.android.shared.repositories.server.client.ITokensProvider
+import kotlin.coroutines.Continuation
 
-abstract class BaseServerRepository<TokensWrapper>(protected val context: Context, private val tokensProvider: ITokensProvider<TokensWrapper>){
-    protected fun getTokens() = tokensProvider.getLocalTokens()
+abstract class BaseServerRepository<TokensWrapper>(val context: Context, val tokensProvider: ITokensProvider<TokensWrapper>){
+    val client = HttpClient(context)
+
+    fun getTokens() = tokensProvider.getLocalTokens()
+
+    suspend inline fun <reified Type>executeRequest(crossinline requestFactoryMethod: (Continuation<Type?>) -> BaseHttpRequest<Type?>): Type?{
+        return client.executeRequest {
+            requestFactoryMethod(it)
+        }
+    }
 }
