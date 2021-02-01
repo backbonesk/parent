0ea2b094a3003f2b499e349688bc1770a5d2b631
Nono3551 <Nono3551@gmail.com>
2021-02-01 15:09:04 +0100

    Job cancel change

--- app/src/main/java/sk/backbone/parent/execution/Scopes.kt
+++ app/src/main/java/sk/backbone/parent/execution/Scopes.kt
@@ -3,6 +3,7 @@ package sk.backbone.parent.execution
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
 
 class Scopes {
     private val uiJob = SupervisorJob()
@@ -15,8 +16,8 @@ class Scopes {
     val default: CoroutineScope get() = CoroutineScope(Dispatchers.Default + defaultJob)
 
     fun cancelJobs(){
-        uiJob.cancel()
-        ioJob.cancel()
-        defaultJob.cancel()
+        uiJob.cancelChildren()
+        ioJob.cancelChildren()
+        defaultJob.cancelChildren()
     }
 }
