55a89cdffd0e58cd45cd49284796385372a6c630
Nono3551 <Nono3551@gmail.com>
2021-02-03 17:06:57 +0100

    Spinner

--- app/src/main/java/sk/backbone/parent/ui/components/ParentSpinner.kt
+++ app/src/main/java/sk/backbone/parent/ui/components/ParentSpinner.kt
@@ -2,13 +2,15 @@ package sk.backbone.parent.ui.components
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import sk.backbone.parent.ui.validations.text_validation.TextInputValidation
 import sk.backbone.parent.utils.hideKeyboard
 
 abstract class ParentSpinner @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : StateSavingLinearLayout(context, attrs, defStyleAttr) {
-    private var onValueChanged: ((String) -> Unit)? = null
+    var onItemSelected: ((ParentSpinner, Int) -> Unit)? = null
     var provider: ISpinnerItemsProvider<*>? = null
         set(value) {
             field = value
@@ -38,6 +40,14 @@ abstract class ParentSpinner @JvmOverloads constructor(context: Context, attrs: 
 
         if(!isEnabled){
             spinner.isEnabled = false
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+
+            override fun onItemSelected(p0: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                onItemSelected?.invoke(this@ParentSpinner, position)
+            }
         }
 
         spinner.adapter = this.context?.let { ArrayAdapter(it, spinnerItemResource, arrayOf("")) }
