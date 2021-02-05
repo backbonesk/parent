ed252fe15e2c8c1eec982c57d5865306d5f51b95
Nono3551 <Nono3551@gmail.com>
2021-02-05 10:49:42 +0100

    Added recycler adapter

---
+++ app/src/main/java/sk/backbone/parent/ui/screens/ParentRecyclerAdapter.kt
@@ -0,0 +1,26 @@
+package sk.backbone.parent.ui.screens
+
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class ParentRecyclerAdapter<TType, TViewHolder>(protected val context: Context): RecyclerView.Adapter<TViewHolder>() where TViewHolder: ParentRecyclerAdapter.BaseRecyclerViewHolder<TType> {
+    protected val dataSet = mutableListOf<TType>()
+    abstract override fun getItemId(position: Int): Long
+
+    override fun getItemCount() = dataSet.size
+
+    override fun onBindViewHolder(viewHolder: TViewHolder, position: Int) {
+        viewHolder.bindData(dataSet[position])
+    }
+
+    fun replaceDataSet(newDataSet: List<TType>?){
+        dataSet.clear()
+        newDataSet?.let { dataSet.addAll(it) }
+        notifyDataSetChanged()
+    }
+
+    abstract class BaseRecyclerViewHolder<TDataType>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        abstract fun bindData(viewData: TDataType)
+    }
+}
