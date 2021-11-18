9781789896942878c6ee9da756bd57c11dcc751e
Michal Záhradník <Nono3551@gmail.com>
2021-11-18 10:09:20 +0100

    Image utils

--- app/src/main/java/sk/backbone/parent/utils/ImageUtils.kt
+++ app/src/main/java/sk/backbone/parent/utils/ImageUtils.kt
@@ -7,28 +7,28 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Base64
 import androidx.core.content.FileProvider
 import androidx.exifinterface.media.ExifInterface
 import com.bumptech.glide.load.resource.bitmap.TransformationUtils
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.util.*
 
-private const val IMAGE_FILE_NAME = "_parent_last_stored_image.png"
+private const val IMAGE_FILE_NAME = "parent_image_%d.png"
 
-val Context.lastStoredImageFile: File? get() {
+fun Context.getParentImageFile(timestamp: Long): File? {
     return try {
-        File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), IMAGE_FILE_NAME)
+        File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), String.format(IMAGE_FILE_NAME, timestamp))
     } catch (exception: Exception){
         exception.printStackTrace()
         null
     }
 }
 
-fun Context.createCameraIntentForStoringImage(): Intent? {
-    return lastStoredImageFile?.let { pictureFile ->
+fun Context.createParentCameraIntentForStoringImage(): Pair<Long, Intent?> {
+    val currentTime = Date().time
+    return currentTime to getParentImageFile(currentTime)?.let { pictureFile ->
         pictureFile.delete()
 
         Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {
@@ -38,26 +38,29 @@ fun Context.createCameraIntentForStoringImage(): Intent? {
     }
 }
 
-fun Context.storeNewImage(bitmap: Bitmap){
-    try {
-        lastStoredImageFile?.let {
+fun Context.storeNewParentImage(bitmap: Bitmap): Long?{
+    val currentTime = Date().time
+    return try {
+        getParentImageFile(currentTime)?.let {
             it.delete()
             val stream = FileOutputStream(it, false)
             bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
             stream.close()
+            currentTime
         }
     } catch (e: Exception) {
         e.printStackTrace()
+        null
     }
 }
 
-fun Context.getLastStoredImage(): Bitmap? {
+fun Context.getParentImage(identifier: Long): Bitmap? {
     return try {
-        val inputStream = FileInputStream(lastStoredImageFile)
+        val inputStream = FileInputStream(getParentImageFile(identifier))
         val bitmap: Bitmap? = BitmapFactory.decodeStream(inputStream)
         inputStream.close()
 
-        val exifInterface = lastStoredImageFile?.absolutePath?.let { ExifInterface(it) }
+        val exifInterface = getParentImageFile(identifier)?.absolutePath?.let { ExifInterface(it) }
 
         return when (exifInterface?.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)) {
             ExifInterface.ORIENTATION_ROTATE_90 -> TransformationUtils.rotateImage(bitmap!!, 90)
