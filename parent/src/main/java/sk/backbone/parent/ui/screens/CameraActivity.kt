7d03407e28df7988fd698dec56ae9ac4827ed069
Michal Záhradniík <michal.zahradnik@backbone.sk>
2022-02-10 17:09:14 +0100

    Future todo

--- parent/src/main/java/sk/backbone/parent/ui/screens/CameraActivity.kt
+++ parent/src/main/java/sk/backbone/parent/ui/screens/CameraActivity.kt
@@ -7,7 +7,6 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
-import android.content.res.Configuration
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
@@ -27,7 +26,7 @@ import sk.backbone.parent.databinding.ActivityCameraBinding
 import sk.backbone.parent.utils.afterMeasured
 import sk.backbone.parent.utils.setSafeOnClickListener
 
-// Todo: Front/Back camera switching
+// Future Todo: Front/Back camera switching
 
 @AndroidEntryPoint
 class CameraActivity : ParentActivity<ActivityCameraBinding>(ActivityCameraBinding::inflate) {
