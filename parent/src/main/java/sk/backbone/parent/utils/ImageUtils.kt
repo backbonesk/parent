13d4337b7706883a85cd3c9ee9b5383f7bfcf940
Michal Záhradniík <michal.zahradnik@backbone.sk>
2022-02-11 19:17:57 +0100

    Working images in incidents

--- parent/src/main/java/sk/backbone/parent/utils/ImageUtils.kt
+++ parent/src/main/java/sk/backbone/parent/utils/ImageUtils.kt
@@ -57,7 +57,11 @@ fun Context.getParentImageFile(identifier: Any?): File? {
     }
 }
 
-fun createSelectImageFromGalleryIntent(): Intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply { type = "image/*" }
+fun createSelectImageFromGalleryIntent(): Intent = Intent(Intent.ACTION_OPEN_DOCUMENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+    type = "image/*"
+    addCategory(Intent.CATEGORY_OPENABLE)
+    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+}
 
 fun Context.createParentCameraXIntentForStoringImageIntoGallery(context:Context, identifier: Any = Date().time, orientation: Int = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR, @CameraSelector.LensFacing lensFacing: Int = CameraSelector.LENS_FACING_BACK, layoutOverlay: Int? = null): Pair<Any?, Intent?> {
     val photoUri: Uri? = getMediaStorageUri(context, identifier)
