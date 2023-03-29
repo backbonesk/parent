0264b81383dca91bb7992d1668df97233804f1b6
Andrej Macko <a.macko96@gmail.com>
2023-03-29 15:46:21 +0200

    terrible fix but it works

--- parent/src/main/java/sk/backbone/parent/utils/DateUtils.kt
+++ parent/src/main/java/sk/backbone/parent/utils/DateUtils.kt
@@ -12,7 +12,7 @@ const val iso8601FormatUtcStrict = "yyyy-MM-dd'T'HH:mm:ss'Z'"
 const val iso84FormatUtcStrictNoMillis = "yyyy-MM-dd'T'HH:mm:ssZ"
 const val iso8601DateOnlyFormat = "yyyy-MM-dd"
 const val iso8601FormatWithoutSeconds = "yyyy-MM-dd'T'HH:mmZZZZZ"
-const val iso8601TimeOnlyFormat = "HH:mm"
+const val iso8601HoursMinutesOnlyFormat = "HH:mm"
 
 fun getDateWithFirstMonthDay(timezone: TimeZone = TimeZone.getDefault()): Date = Calendar.getInstance(timezone).apply {
     this.set(Calendar.DAY_OF_MONTH, getActualMinimum(Calendar.DAY_OF_MONTH))
@@ -148,8 +148,13 @@ fun formatIsoDateOnlyToLocalDate(inputDate: String?): String? {
 fun Date.getIso8601DateOnlyString(): String {
     return SimpleDateFormat(iso8601DateOnlyFormat, Locale.getDefault()).format(this)
 }
-fun Date.getIso8601TimeOnlyString(): String {
-    return SimpleDateFormat(iso8601TimeOnlyFormat, Locale.getDefault()).format(this)
+fun Date.getLocallyHoursMinutesOnlyString(): String {
+    val format = android.text.format.DateFormat.getBestDateTimePattern(Locale.getDefault(), iso8601HoursMinutesOnlyFormat)
+    val format1 = android.text.format.DateFormat.getBestDateTimePattern(Locale.KOREA, iso8601HoursMinutesOnlyFormat)
+    val format2 = android.text.format.DateFormat.getBestDateTimePattern(Locale.TAIWAN, iso8601HoursMinutesOnlyFormat)
+    val format3 = android.text.format.DateFormat.getBestDateTimePattern(Locale.CANADA, iso8601HoursMinutesOnlyFormat)
+    val format4 = android.text.format.DateFormat.getBestDateTimePattern(Locale.US, iso8601HoursMinutesOnlyFormat)
+    return SimpleDateFormat(format, Locale.getDefault()).format(this)
 }
 
 fun Date.iso84FormatUtcStrictNoMillis(): String {
