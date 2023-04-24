133098bd682548d64c08ab33b9c147be6afbd236
Nono3551 <Nono3551@gmail.com>
2023-04-24 11:54:05 +0200

    New feature - Daily Records

--- parent/src/main/java/sk/backbone/parent/execution/IExecutorDialogProvider.kt
+++ parent/src/main/java/sk/backbone/parent/execution/IExecutorDialogProvider.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AlertDialog
 
 interface IExecutorDialogProvider {
-    fun showDialog(context: Context, message: String, title: String? = null, neutralButton: Pair<String?, ((() -> Unit)?)>? = null, positiveButton: Pair<String, ((() -> Unit)?)>? = null, negativeButton: Pair<String, ((() -> Unit)?)>? = null): AlertDialog?
+    fun showDialog(context: Context, message: CharSequence, title: CharSequence? = null, neutralButton: Pair<CharSequence?, ((() -> Unit)?)>? = null, positiveButton: Pair<CharSequence, ((() -> Unit)?)>? = null, negativeButton: Pair<CharSequence, ((() -> Unit)?)>? = null): AlertDialog?
     fun showLoadingDialog(context: Context): AlertDialog?
-    fun getDefaultNeutralButton(context: Context, action: (() -> Unit)? = null): Pair<String, ((() -> Unit)?)>
+    fun getDefaultNeutralButton(context: Context, action: (() -> Unit)? = null): Pair<CharSequence, ((() -> Unit)?)>
 }
